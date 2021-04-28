6345cf909eb558a08342098fa85214793d60096c
ParangBird <seekerman@naver.com>
2021-04-28 23:27:28 +0900

    - 파이어베이스 구축 - 회원가입, 로그인 기능 구현 및 간단한 디자인

--- app/src/main/java/com/example/mooyaho/LoginActivity.java
+++ app/src/main/java/com/example/mooyaho/LoginActivity.java
@@ -1,15 +1,30 @@
 package com.example.mooyaho;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     Button button;
-
+    Button buttonLogin;
+    EditText editTextEmail;
+    EditText editTextPassword;
+    TextView register;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +35,82 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
+
         button = (Button) findViewById(R.id.button);
+        buttonLogin = (Button) findViewById(R.id.login);
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        register = (TextView) findViewById(R.id.register);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void setButtonClickListener() {
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));             // 버튼 클릭시 MainActivity 로 이동
-                finish();                                                                               // LoginActivity 종료
+            public void onClick(View view) {
+
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() { // 회원 가입 창으로 이동
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() { // 로그인 실행
+            @Override
+            public void onClick(View view) {
+                userLogin();
+            }
+        });
+    }
+
+    private void userLogin() { // 로그인을 담당하는 함수
+        
+        // 각각 string 받아오기
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        
+        // 각각 string 조건 검사
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        
+        // Auth 과정
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) { // 인증 성공
+                if(task.isSuccessful()){
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                    if(user.isEmailVerified()){ // 이메일 인증 됐을 시 메인액티비티로
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+                    else{ // 아니면 안보내줌
+                        Toast.makeText(LoginActivity.this, "이메일 인증 후 다시 눌러주세요",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else{ // 인증 실패
+                    Toast.makeText(LoginActivity.this, "로그인 실패!", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/mooyaho/MainActivity.java
+++ app/src/main/java/com/example/mooyaho/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setButtonClickListener();
     }
 
-    private void initView() {
-        button = (Button) findViewById(R.id.button);
-    }
+    private void initView() { button = (Button) findViewById(R.id.button); }
 
     private void setButtonClickListener() {
         button.setOnClickListener(new View.OnClickListener() {
