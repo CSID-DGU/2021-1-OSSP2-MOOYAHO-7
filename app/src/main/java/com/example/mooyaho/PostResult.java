e6fb92fa0dea94ec38ea780976c0de581a6454d9
ParangBird <seekerman@naver.com>
2021-05-03 22:39:59 +0900

    - retrofit을 통한 node, mySQL 연동

---
+++ app/src/main/java/com/example/mooyaho/PostResult.java
@@ -0,0 +1,36 @@
+package com.example.mooyaho;
+
+public class PostResult { // Post 정보를 서버에서 가져와야 할 때 해당 클래스 형식으로 가져온다
+
+    private String postTitle;
+    private String postContent;
+    private String postLocation;
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    public String getLocation() {
+        return postLocation;
+    }
+
+    public void setLocation(String location) {
+        this.postLocation = location;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/mooyaho/RetrofitInterface.java
@@ -0,0 +1,15 @@
+package com.example.mooyaho;
+
+import java.util.HashMap;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitInterface { // 노드-mysql 연동을 위한 Retrofit 인터페이스
+
+    @POST("/post") // /post로 갈 시 해당 함수를 실행한다
+    Call<Void> executePost(@Body HashMap<String,String> map);
+
+    @POST("/get") // /get으로 갈 시 해당 함수를 실행
+    Call<PostResult> executeGet();
+}
